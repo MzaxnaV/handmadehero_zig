a492d3fbd107496bf611b5e2415834134cb5aa3b
MzaxnaV <manav.zxa@gmail.com>
2021-12-17 22:56:21 +0530

    day 004

--- code/win32_handmade.zig
+++ code/win32_handmade.zig
@@ -5,6 +5,7 @@ const win32 = struct {
     usingnamespace @import("win32").graphics.gdi;
     usingnamespace @import("win32").foundation;
     usingnamespace @import("win32").system.diagnostics.debug;
+    usingnamespace @import("win32").system.memory;
     usingnamespace @import("win32").ui.windows_and_messaging;
     usingnamespace @import("win32").zig;
 };
@@ -14,28 +15,53 @@ var running: bool = undefined;
 
 var bitmapInfo: win32.BITMAPINFO = undefined;
 var bitmapMemory: ?*c_void = undefined;
-var bitmapHandle: ?win32.HBITMAP = undefined;
-var bitmapDeviceContext: ?win32.HDC = undefined;
+var bitmapWidth: i32 = 0;
+var bitmapHeight: i32 = 0;
+var bytesPerPixel: u8 = 4;
 
-fn ResizeDIBSection(width: i32, height: i32) void {
+fn RenderWeirdGradient(xOffset: i32, yOffset: i32) void {
+    // var width = bitmapWidth;
+    // var height = bitmapHeight;
+
+    var pitch = @intCast(usize, bitmapWidth * bytesPerPixel);
+    var row = @ptrCast([*]u8, bitmapMemory);
+    var y: u32 = 0;
+
+    while (y < bitmapHeight) {
+        var x: u32 = 0;
+        var pixel = @ptrCast([*]u32, @alignCast(4, row));
+        while (x < bitmapWidth) {
+            // Pixel in memory: BB GG RR xx
+            // Little endian arch: 0x xxRRGGBB
+            var blue = x + @intCast(u32, xOffset);
+            var green = y + @intCast(u32, yOffset);
+
+            var color = (green << 8) | blue;
+
+            pixel.* = color;
+            pixel += 1;
+            x += 1;
+        }
+        row += pitch;
+        y += 1;
+    }
+}
+
+fn Win32ResizeDIBSection(width: i32, height: i32) void {
     // TODO: Bullet proof this.
     // Maybe don't free	first, free after, then free first if that failes.
 
-    // TODO: Free out dibsection
-
-    if (bitmapHandle != null) {
-        _ = win32.DeleteObject(bitmapHandle);
+    if (bitmapMemory != null) {
+        _ = win32.VirtualFree(bitmapMemory, 0, win32.MEM_RELEASE);
     }
 
-    if (bitmapDeviceContext == null) {
-        //TODO: Should we recreate these under special circumstances
-        bitmapDeviceContext = win32.CreateCompatibleDC(null);
-    }
+    bitmapWidth = width;
+    bitmapHeight = height;
 
     bitmapInfo = win32.BITMAPINFO{ .bmiHeader = win32.BITMAPINFOHEADER{
         .biSize = @sizeOf(win32.BITMAPINFOHEADER),
-        .biWidth = width,
-        .biHeight = height,
+        .biWidth = bitmapWidth,
+        .biHeight = bitmapHeight,
         .biPlanes = 1,
         .biBitCount = 32,
         .biCompression = win32.BI_RGB,
@@ -51,27 +77,31 @@ fn ResizeDIBSection(width: i32, height: i32) void {
         .rgbReserved = 0,
     }} };
 
-    bitmapHandle = win32.CreateDIBSection(
-        bitmapDeviceContext,
-        &bitmapInfo,
-        win32.DIB_RGB_COLORS,
-        &bitmapMemory,
-        null,
-        0,
-    );
+    const bitmapMemorySize = @intCast(usize, bytesPerPixel * (width * height));
+    bitmapMemory = win32.VirtualAlloc(null, bitmapMemorySize, win32.MEM_COMMIT, win32.PAGE_READWRITE);
+
+    // TODO: probably clear this to black
 }
 
-fn UpdateWindow(deviceContext: win32.HDC, x: i32, y: i32, width: i32, height: i32) void {
-    win32.StretchDIBits(
+fn Win32UpdateWindow(deviceContext: win32.HDC, clientRect: *win32.RECT, x: i32, y: i32, width: i32, height: i32) void {
+    _ = x;
+    _ = y;
+    _ = width;
+    _ = height;
+
+    var windowWidth = clientRect.right - clientRect.left;
+    var windowHeight = clientRect.bottom - clientRect.top;
+
+    _ = win32.StretchDIBits(
         deviceContext,
-        x,
-        y,
-        width,
-        height,
-        x,
-        y,
-        width,
-        height,
+        0,
+        0,
+        bitmapWidth,
+        bitmapHeight,
+        0,
+        0,
+        windowWidth,
+        windowHeight,
         bitmapMemory,
         &bitmapInfo,
         win32.DIB_RGB_COLORS,
@@ -88,11 +118,15 @@ fn WindowProc(windowHandle: win32.HWND, message: u32, wParam: win32.WPARAM, lPar
             _ = win32.GetClientRect(windowHandle, &clientRect);
             var height = clientRect.bottom - clientRect.top;
             var width = clientRect.right - clientRect.left;
-            ResizeDIBSection(width, height);
+            Win32ResizeDIBSection(width, height);
         },
+
         win32.WM_DESTROY => running = false, // TODO: handle this as an error = recreate window?
+
         win32.WM_CLOSE => running = false, // TODO: handle this with a message to a user?
+
         win32.WM_ACTIVATEAPP => win32.OutputDebugStringA("WM_ACTIVATEAPP\n"),
+
         win32.WM_PAINT => {
             var paint: win32.PAINTSTRUCT = undefined;
             var deviceContext = win32.BeginPaint(windowHandle, &paint);
@@ -100,9 +134,15 @@ fn WindowProc(windowHandle: win32.HWND, message: u32, wParam: win32.WPARAM, lPar
             var y = paint.rcPaint.top;
             var height = paint.rcPaint.bottom - paint.rcPaint.top;
             var width = paint.rcPaint.right - paint.rcPaint.left;
-            _ = win32.PatBlt(deviceContext, x, y, width, height, win32.WHITENESS);
+
+            var clientRect: win32.RECT = undefined;
+            _ = win32.GetClientRect(windowHandle, &clientRect);
+            if (deviceContext) |context| {
+                Win32UpdateWindow(context, &clientRect, x, y, width, height);
+            }
             _ = win32.EndPaint(windowHandle, &paint);
         },
+
         else => result = win32.DefWindowProc(windowHandle, message, wParam, lParam),
     }
 
@@ -110,7 +150,7 @@ fn WindowProc(windowHandle: win32.HWND, message: u32, wParam: win32.WPARAM, lPar
 }
 
 pub export fn wWinMain(hInstance: ?win32.HINSTANCE, _: ?win32.HINSTANCE, _: [*:0]u16, _: u32) callconv(WINAPI) c_int {
-    const windowClass = win32.WNDCLASS{
+    const windowclass = win32.WNDCLASS{
         .style = @intToEnum(win32.WNDCLASS_STYLES, 0),
         .lpfnWndProc = WindowProc,
         .cbClsExtra = 0,
@@ -123,19 +163,34 @@ pub export fn wWinMain(hInstance: ?win32.HINSTANCE, _: ?win32.HINSTANCE, _: [*:0
         .lpszClassName = win32.L("HandmadeHeroWindowClass"),
     };
 
-    if (win32.RegisterClass(&windowClass) != 0) {
-        const windowHandle = win32.CreateWindowEx(@intToEnum(win32.WINDOW_EX_STYLE, 0), windowClass.lpszClassName, win32.L("HandmadeHero"), @intToEnum(win32.WINDOW_STYLE, @enumToInt(win32.WS_OVERLAPPEDWINDOW) | @enumToInt(win32.WS_VISIBLE)), win32.CW_USEDEFAULT, win32.CW_USEDEFAULT, win32.CW_USEDEFAULT, win32.CW_USEDEFAULT, null, null, hInstance, null);
+    if (win32.RegisterClass(&windowclass) != 0) {
+        if (win32.CreateWindowEx(@intToEnum(win32.WINDOW_EX_STYLE, 0), windowclass.lpszClassName, win32.L("HandmadeHero"), @intToEnum(win32.WINDOW_STYLE, @enumToInt(win32.WS_OVERLAPPEDWINDOW) | @enumToInt(win32.WS_VISIBLE)), win32.CW_USEDEFAULT, win32.CW_USEDEFAULT, win32.CW_USEDEFAULT, win32.CW_USEDEFAULT, null, null, hInstance, null)) |windowHandle| {
+            var xOffset: i32 = 0;
+            var yOffset: i32 = 0;
 
-        if (windowHandle != null) {
             running = true;
-            var message: win32.MSG = undefined;
             while (running) {
-                var messageResult = win32.GetMessage(&message, null, 0, 0);
-                if (messageResult > 0) {
+                var message: win32.MSG = undefined;
+                while (win32.PeekMessage(&message, null, 0, 0, win32.PM_REMOVE) != 0) {
+                    if (message.message == win32.WM_QUIT) {
+                        running = false;
+                    }
                     _ = win32.TranslateMessage(&message);
                     _ = win32.DispatchMessage(&message);
-                } else {
-                    break;
+                }
+
+                RenderWeirdGradient(xOffset, yOffset);
+
+                if (win32.GetDC(windowHandle)) |deviceContext| {
+                    var clientRect: win32.RECT = undefined;
+                    _ = win32.GetClientRect(windowHandle, &clientRect);
+
+                    var windowWidth = clientRect.right - clientRect.left;
+                    var windowHeight = clientRect.bottom - clientRect.top;
+
+                    Win32UpdateWindow(deviceContext, &clientRect, 0, 0, windowWidth, windowHeight);
+                    _ = win32.ReleaseDC(windowHandle, deviceContext);
+                    xOffset += 1;
                 }
             }
         } else {
