a116063299cc3d5beadbdf11ba340836a650cdaf
Manav <30342951+MzaxnaV@users.noreply.github.com>
2024-07-03 01:20:31 +0100

    add gen wav tool

--- build.zig
+++ build.zig
@@ -6,11 +6,21 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    // const options = b.addOptions();
-    // options.addOption(bool, "NOTIGNORE", true);
+    // ----------------------------------------------------------------------------------------------------
+    // Options --------------------------------------------------------------------------------------------
+    // ----------------------------------------------------------------------------------------------------
+
+    const options = b.addOptions();
+    // NOTE (Manav)L for now this are independent of OptimizeMode
+    options.addOption(bool, "IGNORE", false);
+    options.addOption(bool, "HANDMADE_INTERNAL", true);
+    options.addOption(bool, "HANDMADE_SLOW", true);
+
+    // ----------------------------------------------------------------------------------------------------
+    // Modules --------------------------------------------------------------------------------------------
+    // ----------------------------------------------------------------------------------------------------
 
     // TODO (Manav): Should I split handmade hero code into small modules?
-
     const platform = b.createModule(.{
         .root_source_file = b.path("./code/handmade_platform.zig"),
     });
@@ -30,6 +40,52 @@ pub fn build(b: *std.Build) void {
 
     const win32 = b.dependency("zigwin32", .{}).module("zigwin32");
 
+    // ----------------------------------------------------------------------------------------------------
+    // Build targets --------------------------------------------------------------------------------------
+    // ----------------------------------------------------------------------------------------------------
+
+    const stb_truetype_data = "#define STB_TRUETYPE_IMPLEMENTATION\n#include <stb_truetype.h>\n";
+    const misc_build_path: std.Build.LazyPath = .{ .src_path = .{ .owner = b, .sub_path = "data" } };
+
+    // ----------------------------------------------------------------------------------------------------
+    // Tools - asset builder ------------------------------------------------------------------------------
+    const asset_builder = b.addExecutable(.{
+        .name = "asset_builder",
+        .root_source_file = b.path("./code/handmade/asset_builder.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    asset_builder.root_module.addImport("win32", win32);
+    asset_builder.root_module.addImport("handmade_platform", platform);
+    asset_builder.root_module.addCSourceFile(.{
+        // NOTE: Need to add a source file to make zig compile the stb_truetype implementation
+        .file = b.addWriteFiles().add("std_truetype.c", stb_truetype_data),
+        .flags = &.{""},
+    });
+    asset_builder.root_module.addIncludePath(b.path("./code/handmade/"));
+    asset_builder.root_module.link_libc = true;
+
+    const asset_builder_install_step = b.addInstallArtifact(asset_builder, .{
+        .dest_dir = .{ .override = .prefix },
+        .pdb_dir = .{ .override = .prefix },
+    });
+    const run_step = b.step("asset", "Build the asset builder");
+    run_step.dependOn(&asset_builder_install_step.step);
+
+    b.getInstallStep().dependOn(&asset_builder_install_step.step);
+
+    // ----------------------------------------------------------------------------------------------------
+    // Tools - generate wav -------------------------------------------------------------------------------
+
+    const gen_wav_tool = b.addSystemCommand(&.{"py"});
+    gen_wav_tool.setCwd(misc_build_path);
+    gen_wav_tool.addFileArg(b.path("code/tools/gen_wav.py"));
+
+    const run_gen_wav_tool = b.step("gen_wav", "Run the wav generator");
+    run_gen_wav_tool.dependOn(&gen_wav_tool.step);
+
+    // ----------------------------------------------------------------------------------------------------
+    // Handmade library -----------------------------------------------------------------------------------
     const lib = b.addSharedLibrary(.{
         .name = lib_name,
         .root_source_file = b.path("./code/handmade/handmade.zig"),
@@ -41,6 +97,38 @@ pub fn build(b: *std.Build) void {
     lib.root_module.addImport("simd", simd);
     lib.root_module.addImport("debug", debug);
 
+    const lib_install_step = b.addInstallArtifact(lib, .{
+        .dest_dir = .{ .override = .prefix },
+        .pdb_dir = .{ .override = .prefix },
+        .implib_dir = .{ .override = .prefix },
+    });
+
+    const build_step = b.step("lib", "Build the handmade lib");
+    build_step.dependOn(&lib_install_step.step);
+    build_step.dependOn(&b.addInstallFile(lib.getEmittedAsm(), "handmade.s").step);
+
+    b.getInstallStep().dependOn(&lib_install_step.step);
+
+    // ----------------------------------------------------------------------------------------------------
+    // Handmade exe ---------------------------------------------------------------------------------------
+    const exe = b.addExecutable(.{
+        .name = "win32_handmade",
+        .root_source_file = b.path("./code/win32_handmade.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.root_module.addImport("win32", win32);
+    exe.root_module.addImport("handmade_platform", platform);
+
+    const exe_install_step = b.addInstallArtifact(exe, .{
+        .dest_dir = .{ .override = .prefix },
+        .pdb_dir = .{ .override = .prefix },
+    });
+
+    b.getInstallStep().dependOn(&exe_install_step.step);
+
+    // ----------------------------------------------------------------------------------------------------
+    // Tests ----------------------------------------------------------------------------------------------
     const lib_tests = b.addTest(.{
         .root_source_file = b.path("./code/handmade/handmade_tests.zig"),
         .target = target,
@@ -53,57 +141,4 @@ pub fn build(b: *std.Build) void {
 
     const test_step = b.step("test", "Run handmade tests");
     test_step.dependOn(&run_test.step);
-
-    const exe = b.addExecutable(.{
-        .name = "win32_handmade",
-        .root_source_file = b.path("./code/win32_handmade.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    exe.root_module.addImport("win32", win32);
-    exe.root_module.addImport("handmade_platform", platform);
-
-    const stb_truetype_data = "#define STB_TRUETYPE_IMPLEMENTATION\n#include <stb_truetype.h>\n";
-
-    const asset_builder = b.addExecutable(.{
-        .name = "asset_builder",
-        .root_source_file = b.path("./code/handmade/asset_builder.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    asset_builder.root_module.addImport("win32", win32);
-    asset_builder.root_module.addImport("handmade_platform", platform);
-    asset_builder.root_module.addCSourceFile(.{ // NOTE: Need to add a source file to make zig compile the stb_truetype implementation
-        .file = b.addWriteFiles().add("std_truetype.c", stb_truetype_data),
-        .flags = &.{""},
-    });
-    asset_builder.root_module.addIncludePath(b.path("./code/handmade/"));
-    asset_builder.root_module.link_libc = true;
-
-    const exe_install_step = b.addInstallArtifact(exe, .{
-        .dest_dir = .{ .override = .prefix },
-        .pdb_dir = .{ .override = .prefix },
-    });
-
-    const lib_install_step = b.addInstallArtifact(lib, .{
-        .dest_dir = .{ .override = .prefix },
-        .pdb_dir = .{ .override = .prefix },
-        .implib_dir = .{ .override = .prefix },
-    });
-    const build_step = b.step("lib", "Build the handmade lib");
-    build_step.dependOn(&lib_install_step.step);
-
-    const asset_builder_install_step = b.addInstallArtifact(asset_builder, .{
-        .dest_dir = .{ .override = .prefix },
-        .pdb_dir = .{ .override = .prefix },
-    });
-    const run_step = b.step("asset", "Build the asset builder");
-    run_step.dependOn(&asset_builder_install_step.step);
-
-    const asm_install_step = b.addInstallFile(lib.getEmittedAsm(), "handmade.s");
-
-    b.getInstallStep().dependOn(&lib_install_step.step);
-    b.getInstallStep().dependOn(&asm_install_step.step);
-    b.getInstallStep().dependOn(&asset_builder_install_step.step);
-    b.getInstallStep().dependOn(&exe_install_step.step);
 }
--- code/tools/gen_wav.py
+++ code/tools/gen_wav.py
@@ -3,7 +3,7 @@ import wave
 num_channels = 2
 sample_width = 2  # 2 bytes per sample (16 bits)
 frame_rate = 48000
-duration = 60  # duration in seconds
+duration = 1  # duration in seconds
 num_samples = frame_rate * duration
 
 left_channel_data = bytearray(num_samples * sample_width)
@@ -11,19 +11,19 @@ right_channel_data = bytearray(num_samples * sample_width)
 
 for i in range(0, len(left_channel_data), sample_width):
     # Assign even values (0x00, 0x02, 0x04, etc.) for the left channel
-    value = (i // 2) % 256
-    left_channel_data[i : i + sample_width] = value.to_bytes(2, byteorder="little")
+    left = i % 256
+    left_channel_data[i : i + sample_width] = left.to_bytes(2, byteorder="little")
 
     # Assign odd values (0x01, 0x03, 0x05, etc.) for the right channel
-    value = ((i // 2) + 1) % 256
-    right_channel_data[i : i + sample_width] = value.to_bytes(2, byteorder="little")
+    right = (i + 1) % 256
+    right_channel_data[i : i + sample_width] = right.to_bytes(2, byteorder="little")
 
 interleaved_data = bytearray()
 for i in range(0, len(left_channel_data), sample_width):
     interleaved_data.extend(left_channel_data[i : i + sample_width])
     interleaved_data.extend(right_channel_data[i : i + sample_width])
 
-with wave.open("../../data/wave_stereo_test_1min.wav", "wb") as f:
+with wave.open(f"wave_stereo_test_{duration}sec.wav", "wb") as f:
     f.setnchannels(num_channels)
     f.setsampwidth(sample_width)
     f.setframerate(frame_rate)
